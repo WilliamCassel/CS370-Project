3c554c6d96f2d25b6875bb5738e647b4ad325b22
Daniel Hernandez Castillo <herna1003@csusm.edu>
2023-10-30 11:46:41 -0700

    Got Euclidean distance functionality to work on test data

--- tests/data_test.py
+++ tests/data_test.py
@@ -7,13 +7,22 @@ def main():
     datasetPath = 'test_data/features_raw.csv'
     dataFrame = pd.read_csv(datasetPath)
 
-    user1 = dataFrame.iloc[0]
-    user2 = dataFrame.iloc[1]
+    #choosing rows
+    user1 = dataFrame.iloc[10] #row 0
+    user2 = dataFrame.iloc[15] #row 1
 
     array1 = np.array(user1)
     array2 = np.array(user2)
 
-    distance = calcEDist(array1, array2)
+    #create filter to check for real numbers
+    validInputs = (~np.isnan(array1)) & (~np.isnan(array2)) & np.isreal(array1) & np.isreal(array2)
+    
+    #apply filter
+    filteredArray1 = array1[validInputs]
+    filteredArray2 = array2[validInputs]
+
+    #only works in terminal with this command: python -m tests.data_test not from within vscode
+    distance = calcEDist(filteredArray1, filteredArray2)
 
     print("Distance: " + str(distance))
 
