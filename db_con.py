f269e39f7f729a8a52e50de822265dcc1a08899b
JRyan109 <ryan109@csusm.edu>
2023-11-05 19:38:53 -0800

    Update db_con.py

--- db_con.py
+++ db_con.py
@@ -3,6 +3,7 @@ import sqlite3
 
 def get_db():
     return sqlite3.connect("database.db")
+    return sqlite3.connect("db.sqlite")
 
 def get_db_instance():
     db = get_db()
@@ -13,5 +14,5 @@ def get_db_instance():
 if __name__ == '__main__':
     db, cur = get_db_instance()
 
-    cur.execute("create table users ( username varchar(50), email varchar(50), password varchar(50) );")
+    cur.execute("create table users ( username varchar(50), email varchar(50), password varchar(50), hb_data real )")
     db.commit()
