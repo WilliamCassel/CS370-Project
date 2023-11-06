6ccca417a02e60abdad33e156c37bfab489af364
JRyan109 <ryan109@csusm.edu>
2023-11-05 19:39:41 -0800

    When video stopped, dumps pickled data

--- open_calls/stop_movie.py
+++ open_calls/stop_movie.py
@@ -1,9 +1,18 @@
 from flask import request, g
 from neurosdk.cmn_types import *
+from tools.eeg import *
+from app_updated import update_db
+import pickle
+
 
 def handle_req():
     if g.hb == None:
         return ["Sensor stopped"]
     
     g.hb.exec_command(SensorCommand.CommandStopSignal)
+    with open("hb_data.pickle", "wb") as file:
+        pickle.dump(hb_data, file)
+
+    update_db()
     return ["Sensor Stopped"]
+
