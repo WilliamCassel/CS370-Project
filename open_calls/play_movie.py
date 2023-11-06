3c849dc036f0fc6251d37cc2ed1923040e6f80ef
JRyan109 <ryan109@csusm.edu>
2023-11-05 19:39:10 -0800

    Update play_movie.py

--- open_calls/play_movie.py
+++ open_calls/play_movie.py
@@ -1,10 +1,12 @@
 from flask import request, g
 from neurosdk.cmn_types import *
 from tools.logging import logger
+import pickle
+
 
 def handle_req():
     if g.hb == None:
-        return ["Data Flowing"]
+        return ["No Headband, No Data"]
     
     g.hb.exec_command(SensorCommand.CommandStartSignal)
     return ["Data Flowing"]
