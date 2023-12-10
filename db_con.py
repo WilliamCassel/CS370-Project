e5c79f33262c8e8234433a25e0a69f6a7eacf3f0
danielhcg <35074368+danielhcg@users.noreply.github.com>
2023-12-10 02:16:52 -0800

    commented db_con.py

--- db_con.py
+++ db_con.py
@@ -1,10 +1,13 @@
 import sqlite3
 
+#setting up the database
 
+#connect to db
 def get_db():
     #return sqlite3.connect("database.db")
     return sqlite3.connect("db.sqlite")
 
+#gets db cursor and connection objects
 def get_db_instance():
     db = get_db()
     cur = db.cursor()
@@ -13,6 +16,6 @@ def get_db_instance():
 
 if __name__ == '__main__':
     db, cur = get_db_instance()
-
+    #create table with attributes
     cur.execute("create table users ( username varchar(50), email varchar(50), password varchar(50), hb_data real )")
     db.commit()
